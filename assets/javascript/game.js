a6c508756b47d7efcb5c5b196d8f50a2e185aa1f
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-05-17 20:30:23 -0500

    Added logic for game continue with multiple words & prompt display after win/lose to continue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,115 @@
 
-
-//currentRound('roman');
-
-
-//function currentRound(curWord)
-//{	
-  var dictionary=['jarvis','hammer','asgardian'];
+/*Define & initialize Global variables defining dictionary & wins that is 
+for the entire game*/
+  var dictionary=['jarvis','hammer','asgard', 'tesseract', 
+'infinitystones', 'vibranium', 'ultron', 'hydra'];
   var wins=0;
 
-	var dispWord=null;
-	var letters=[];
-  var pst=[];
-	var guesses = 7;
-	var ltr;
-  var curWord='assist';
-  var isWin=null;
-	
-  dispWord=Array(curWord.length+1).join('_');
-  play.innerHTML=dispWord.split("").join(" ");
-  lives.innerHTML=guesses;
-  win.innerHTML=wins;
-      
+/*Define Global variables that would be used for each round of  
+play*/
+  var dispWord, letters, pst, guesses, ltr, curWord,
+  isWin;
+  
+/*Play Hangman*/
+playHangman();
+
+/*Start the game*/
+function playHangman()
+{
+	currentRoundInitialize();
+       
   document.onkeyup = function(e)
   {
     ltr=e.key.toLowerCase();
     if(guesses>0 && isWin!=true && isAlpha(ltr))
     {
-       currentRoundInput(ltr);
+       currentRoundPlay(ltr);
+    }
+    else if(isWin===true || guesses===0)
+    {
+        currentRoundInitialize();        
     }
   }
-    	
-//}	
+} 	
 
-/*Function for current round of play*/
-function currentRoundInput(ltr)
-  {  
+
+/*Function for initializing variables & display for 
+current round of play*/
+
+function currentRoundInitialize()
+{
+  dispWord=null;
+  letters=[];
+  pst=[];
+  guesses = 7;
+  ltr;
+  curWord=dictionary.pop(); 
+  isWin=null;
+
+  dispWord=Array(curWord.length+1).join('_');
+  play.innerHTML=dispWord.split("").join(" ");
+  lives.innerHTML=guesses;
+  win.innerHTML=wins;
+  letter.innerHTML='';
+  wantmore.innerHTML="";
+  theword.innerHTML="";
+}
+
+/*Function to calculate wins & lives for current
+round of play*/
+
+function currentRoundPlay(ltr)
+{  
     
+    //finds the position of current input in play word
     pst=findPosition(curWord, ltr);
+
+
+    //checks to see if keyin is not already pressed
     if ((!(dispWord.includes(ltr)))&&(!(letters.includes(ltr))))
     {      
+
+        //if keyin is part of current word adds it to display word
         if (pst.length>0)
          {
            dispWord=displayWord(dispWord, pst, ltr);
         }
+
+        //if keyin is not part of current word adds it to letters missed and reduces lives
         else if(!(letters.includes(ltr))){
           guesses=guesses-1;
           letters.push(ltr);
         }
     }
     
+    //calculates the win if display and current word matches
     if(dispWord===curWord)
     {
       wins=wins+1;
       win.innerHTML=wins;
       isWin=true;
     }
+
+    //calls the function to update the display for current keyin
+    currentRoundDisplay();
+}
+
+/*Function to update display for current round*/
+
+function currentRoundDisplay()  
+{
     letter.innerHTML=letters.join(" ").toUpperCase();
     play.innerHTML=dispWord.split("").join(" ").toUpperCase();
-    lives.innerHTML=guesses;   
-
-  }
+    lives.innerHTML=guesses;  
+    if(isWin===true || guesses===0)
+    {
+        wantmore.innerHTML="Press Any key to continue";
+        if(guesses===0)
+        {
+          theword.innerHTML="Sorry you lost, it was "+curWord.toUpperCase();
+        }
+    } 
+}
 
 
 /*Function to compare input characters with the play word 
@@ -78,7 +128,7 @@ function findPosition(word, letter)
   return position;
 }
 
-/*Function to display the play word with current guesses*/
+/*Function to form the play word with current letter*/
 
 function displayWord(dispWord, position, ltr)
 {
@@ -99,11 +149,5 @@ function isAlpha(ltr){
   return(aplha.split("").includes(ltr));
 }
 
-/*Function to keep track of words already displayed in
-the game*/
 
-
-/*Keep track of Wins, Lives and Letters already entered*/
-
-
-
+/*Function to display Hangman Figure*/
